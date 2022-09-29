c2bc93e7293b13d2fe2b98909de316bb7fd71782
kkqy <kuokongqingyun@gmail.com>
2022-09-29 08:49:09 +0800

    simplify name of function

--- slice/slice.go
+++ slice/slice.go
@@ -1,7 +1,7 @@
 package slice
 
 // 判断元素是否在切片中
-func InSlice[T comparable](item T, slice []T) bool {
+func In[T comparable](item T, slice []T) bool {
 	for _, v := range slice {
 		if v == item {
 			return true
